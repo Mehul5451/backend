ec3da9c6fd328aa7f8f0a802eb7cdbabaed8275f
mehul <gadhadaramehul786@gmail.com>
2025-04-20 13:03:36 +0530

    Add adminlogin and connectivity backend

--- adminlogin/conn.js
+++ adminlogin/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config(); // Load from .env
 
 
-mongoose.connect("mongodb+srv://gadhadaramehul786:Mehul284652@@cluster0.vsz5ku5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://gadhadaramehul786:5451@cluster0.vsz5ku5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("MongoDB connected");
   })
