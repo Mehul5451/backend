5483766d1531095b04adbd1954f46b5cd4968e18
mehul <gadhadaramehul786@gmail.com>
2025-04-21 11:16:35 +0530

    ri

--- adminlogin/conn.js
+++ adminlogin/conn.js
@@ -1,11 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 require("dotenv").config(); // Load from .env
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch((err) => console.error('❌ MongoDB connection error:', err));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
