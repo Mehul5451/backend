116881b0f51090e2fc6a8feadd7fd235d71b0c5b
mehul <gadhadaramehul786@gmail.com>
2025-04-26 09:59:00 +0530

    commitede

--- conn.js
+++ conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 // const { Event } = require("../adminlogin/login"); // ✅ Correct relative path
 
 
-mongoose.connect("mongodb://localhost:27017/DJBOOKING")
+mongoose.connect("mongodb+srv://mehul:5451@cluster0.4awickr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => {
   console.log("MongoDB connected");
 }).catch((err) => {
